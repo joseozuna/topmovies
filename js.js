164d5b7915a73f337002fc869aeef4a290b1f707
JoseOzuna <joseeliasgonzalezozuna@gmail.comgir config --global user.name JoseOzunagit config --global user.name JoseOzunagit config user.email joseeliasgonzalezozuna>
2024-02-09 15:55:53 -0600

    Añadir archivos de la carpeta Peliculas

---
+++ js.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var boton = document.getElementById("miBoton");
+    boton.addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+});
